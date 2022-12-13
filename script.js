0b311770da9b6c440dd796073fe46ef6fcfc7210
James Crutchley <jamescrutchley98@gmail.com>
2022-12-13 13:44:03 +1100

    Create basic sketchpad

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
 const sketchContainer = document.querySelector('.container');
+const controls = document.querySelector('.controls');
+let gridSize = 16;
+
 
 const draw = (e) => {
     let target = e.target;
-    target.style.backgroundColor === 'red' ? 
-        target.style.backgroundColor = 'blue' :
-        target.style.backgroundColor = 'red';
+    target.style.backgroundColor === 'white' ? 
+        target.style.backgroundColor = 'black' :
+        target.style.backgroundColor = 'white';
 }
 
 const createSketchPad = () => {
     
     pageWidth = window.innerWidth;
     pageHeight = window.innerHeight;
-    sketchContainer.style.width = pageWidth/2;
-    sketchContainer.style.height = pageHeight/2;
+    sketchContainer.style.width = '600px';
+    sketchContainer.style.height = '600px';
+
+    // create slider
+    const slider = document.createElement('input');
+    controls.appendChild(slider);
+    slider.addEventListener('change', newGrid)
+    slider.type = 'range';
+    slider.min = '4';
+    slider.max = '100';
 
     //create sketchpad
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < 256; i++) {
         let cell = document.createElement(`div`);
         cell.classList.add(`cell`)
         cell.classList.add(`${i}`)
-        cell.style = `width: ${pageWidth/9}; height: ${pageWidth/9}; background-color: blue; border: solid white 2px;`;
+        cell.style = `width: ${600/gridSize}px; height: ${600/gridSize}px; background-color: white; border: solid black 1px;`;
         sketchContainer.appendChild(cell);
     
         cell.addEventListener('mouseover',draw)
     }    
 }
 
-const refresh = () => {
-    let cells = document.querySelectorAll('.cell');
-    pageWidth = window.innerWidth;
-    pageHeight = window.innerHeight;
-    sketchContainer.style.width = pageWidth/2;
-    sketchContainer.style.height = pageHeight/2;
+const newGrid = (e) => {
+    sketchContainer.innerHTML='';
+    let gridSize = e.target.value;
 
-    cells.forEach(cell => {
-        cell.style.width = pageWidth/9;
-        cell.style.height = pageWidth/9;
-    });
+    for (i = 0; i < gridSize*gridSize; i++) {
+        let cell = document.createElement(`div`);
+        cell.classList.add(`cell`)
+        cell.classList.add(`${i}`)
+        cell.style = `width: ${600/gridSize}px; height: ${600/gridSize}px; background-color: white; border: solid black 1px;`;
+        sketchContainer.appendChild(cell);
     
+        cell.addEventListener('mouseover',draw)
+    }    
 }
 
-
 createSketchPad()
-window.addEventListener('resize', refresh)
